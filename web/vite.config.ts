97e151ac5bbd7de1580b83c3cff696903898e562
Nguyễn Văn Sơn <sonhellonguyen1000@gmail.com>
2026-07-06 16:10:36 +0700

    fix: remove custom domain CNAME and configure base path for github pages

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -6,6 +6,7 @@ import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
+    base: process.env.GITHUB_ACTIONS ? "/PBL3_busTickets/" : "/",
     plugins: [react(), tailwindcss()],
     resolve: {
         alias: {
